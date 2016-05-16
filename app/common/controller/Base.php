b2d836503cd3925d098cbb6ca50a252fec5d5eed
mikexie <1413519894@qq.com>
2016-05-16 12:42:25 +0800

    更新

--- app/common/controller/Base.php
+++ app/common/controller/Base.php
@@ -14,7 +14,8 @@ class Base extends Controller {
 
     public function _initialize() {
         if (!session('uid') || !session('user')) {
-            return $this->error('还未登陆，请登录', url('admin/login/login'));
+            echo $this->error('尚未登陆，请登录', url('admin/login/login'));
+            exit();
         }
     }
 
